b880dd441fcb01df1e82998287ca265e375acc42
isaiaspadovezzi <padovezzi@gmail.com>
2026-07-29 19:30:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,40 @@ navigator.clipboard.writeText(texto);
 
 alert("Copiado!");
 }
+const slides=document.querySelectorAll(".slide");
+
+let atual=0;
+
+mostrar();
+
+function mostrar(){
+
+slides.forEach(s=>s.classList.remove("active"));
+
+slides[atual].classList.add("active");
+
+}
+
+proximo.onclick=()=>{
+
+if(atual<slides.length-1){
+
+atual++;
+
+mostrar();
+
+}
+
+}
+
+voltar.onclick=()=>{
+
+if(atual>0){
+
+atual--;
+
+mostrar();
+
+}
+
+}
